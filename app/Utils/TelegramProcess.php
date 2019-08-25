af21872576efad421df66aa1218db65db4828f69
maxitio <soymaxitio@gmail.com>
2019-08-26 01:13:37 +0800

    修复未绑定用户加入TG被加入黑名单,后面就算绑定了不能加入的bug

--- app/Utils/TelegramProcess.php
+++ app/Utils/TelegramProcess.php
@@ -286,7 +286,7 @@ class TelegramProcess
 
                     if ($user == null && $message->getNewChatMember() != null && MalioConfig::get('force_user_to_bind_tg_when_join_group') == true) {
                         $remove_message = $bot->sendMessage($message->getChat()->getId(), '您未绑定本站账号，无法加入此群组。您可以进入网站的 “我的账号” 页面绑定您的账号。', $parseMode = null, $disablePreview = false, $replyToMessageId = $reply_to, $replyMarkup = $reply['mark']);
-                        $bot->kickChatMember($message->getChat()->getId(), $message->getFrom()->getId());
+                        $bot->kickChatMember($message->getChat()->getId(), $message->getFrom()->getId(), time()+60);
                         $bot->deleteMessage($message->getChat()->getId(), $remove_message->getMessageId());
                     }
             }
