30e7577b86f7366fcd49a22f4c2b5342be8e884c
GeekQu <iloves@live.com>
2019-11-01 19:00:44 +0800

    Create PortAutoChange.php

---
+++ app/Command/PortAutoChange.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Command;
+
+use App\Models\Node;
+use App\Models\User;
+use App\Utils\Telegram;
+use App\Utils\Tools;
+use App\Services\Config;
+use App\Services\Mail;
+use Exception;
+
+class PortAutoChange
+{
+    /**
+     *  配置
+     */
+    private const Config = [
+    ];
+}
