aded91b730226c58a5a4e6d7b711432cea6ce8c2
Rico <rico93@outlook.com>
2019-11-17 21:21:49 +0800

    增加开关是否验证node ip

--- app/Middleware/Mod_Mu.php
+++ app/Middleware/Mod_Mu.php
@@ -30,13 +30,17 @@ class Mod_Mu
             }
         }
 
-        $node = Node::where('node_ip', 'LIKE', $_SERVER['REMOTE_ADDR'] . '%')->first();
-        if ($node == null && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
-            $res['ret'] = 0;
-            $res['data'] = 'token or source is invalid, Your ip address is ' . $_SERVER['REMOTE_ADDR'];
-            $response->getBody()->write(json_encode($res));
-            return $response;
+        if (Config::get('newIndex') == 'true'){
+
+            $node = Node::where('node_ip', 'LIKE', $_SERVER['REMOTE_ADDR'] . '%')->first();
+            if ($node == null && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+                $res['ret'] = 0;
+                $res['data'] = 'token or source is invalid, Your ip address is ' . $_SERVER['REMOTE_ADDR'];
+                $response->getBody()->write(json_encode($res));
+                return $response;
+            }
         }
+
 
         if ($auth == false) {
             $res['ret'] = 0;
