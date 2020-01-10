758b4272ff1aed9bd439167e427b754c37e2c99f
GeekQu <iloves@live.com>
2020-01-10 09:59:21 +0800

    Update glzjin_all.sql

--- sql/glzjin_all.sql
+++ sql/glzjin_all.sql
@@ -732,3 +732,23 @@ CREATE TABLE IF NOT EXISTS `detect_ban_log` (
 ALTER TABLE `user` ADD `detect_ban` int(2) NOT NULL DEFAULT '0' AFTER `enable`;
 ALTER TABLE `user` ADD `last_detect_ban_time` datetime DEFAULT '1989-06-04 00:05:00' AFTER `detect_ban`;
 ALTER TABLE `user` ADD `all_detect_number` int(11) NOT NULL DEFAULT '0' AFTER `last_detect_ban_time`;
+
+
+--
+-- Telegram 任务列表
+--
+CREATE TABLE IF NOT EXISTS `telegram_tasks` (
+  `id`           int(11) unsigned NOT     NULL AUTO_INCREMENT,
+  `type`         int(8)           NOT     NULL                COMMENT '任务类型',
+  `status`       int(2)           NOT     NULL DEFAULT '0'    COMMENT '任务状态',
+  `chatid`       varchar(128)     NOT     NULL DEFAULT '0'    COMMENT 'Telegram Chat ID',
+  `messageid`    varchar(128)     NOT     NULL DEFAULT '0'    COMMENT 'Telegram Message ID',
+  `content`      text             DEFAULT NULL                COMMENT '任务详细内容',
+  `process`      varchar(32)      DEFAULT NULL                COMMENT '临时任务进度',
+  `userid`       int(11)          NOT     NULL DEFAULT '0'    COMMENT '网站用户 ID',
+  `tguserid`     varchar(32)      NOT     NULL DEFAULT '0'    COMMENT 'Telegram User ID',
+  `executetime`  bigint(20)       NOT     NULL                COMMENT '任务执行时间',
+  `datetime`     bigint(20)       NOT     NULL                COMMENT '任务产生时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Telegram 任务列表';
+
